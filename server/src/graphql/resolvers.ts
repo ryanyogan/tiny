75b30a8e1ade9c96387059ea32fa3f8bc39d4673
ryanyogan <ryanyogan@gmail.com>
2020-03-21 09:45:47 -0700

    switched to gql schema syntax

---
+++ server/src/graphql/resolvers.ts
@@ -0,0 +1,24 @@
+import { IResolvers } from "apollo-server-express";
+import { Listing as ListingInterface, listings } from "../listings";
+
+export const resolvers: IResolvers = {
+  Query: {
+    listings: (): ListingInterface[] => {
+      return listings;
+    }
+  },
+  Mutation: {
+    deleteListing: (
+      _root: undefined,
+      { id }: { id: string }
+    ): ListingInterface => {
+      for (let i = 0; i < listings.length; i++) {
+        if (listings[i].id === id) {
+          return listings.splice(i, 1)[0];
+        }
+      }
+
+      throw new Error("failed to delete listing");
+    }
+  }
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,11 @@
 import { ApolloServer } from "apollo-server-express";
 import express from "express";
-import { schema } from "./graphql";
+import { resolvers, typeDefs } from "./graphql";
 
 const app = express();
 const port = 9000;
 
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app, path: "/api" });
 
 app.listen(port);
